b2c16be55b0dc7ef4abb4e4d10ccaea62c0792d0
Ondrej Dockal <odockal@redhat.com>
2025-04-01 13:40:44 +0200

    ci: enable only e2e test
    Signed-off-by: Ondrej Dockal <odockal@redhat.com>

--- tests/playwright/src/utility/operations.ts
+++ tests/playwright/src/utility/operations.ts
@@ -21,6 +21,7 @@ import { execSync } from 'node:child_process';
 import type { Locator, Page } from '@playwright/test';
 import test, { expect as playExpect } from '@playwright/test';
 
+import type { ExtensionType } from '../model/core/extensions';
 import { ResourceElementActions } from '../model/core/operations';
 import { ResourceElementState } from '../model/core/states';
 import { CLIToolsPage } from '../model/pages/cli-tools-page';
@@ -382,4 +383,12 @@ export async function untagImagesFromPodman(name: string, tag: string = ''): Pro
       throw new Error(`Error untagging images from Podman: ${error}`);
     }
   });
+}
+
+export async function disableExtension(page: Page, extension: ExtensionType): Promise<void> {
+  const navBar = new NavigationBar(page);
+  const extensions = await navBar.openExtensions();
+  await extensions.extensionIsInstalled(extension.extensionLabel);
+  const extensionCard = await extensions.getInstalledExtension(extension.extensionName, extension.extensionLabel);
+  await extensionCard.disableExtension();
 }
